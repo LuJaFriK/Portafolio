3a8817b3f1e7330ec9b0e746900de5243f5bd916
LuJaFriK <donfrijolex@gmail.com>
2025-08-27 19:06:59 -0600

    tarea hecha

--- Java/Data Structures and Algorithms/Actividad 1.1/src/main/java/org/example/Main.java
+++ Java/Data Structures and Algorithms/Actividad 1.1/src/main/java/org/example/Main.java
@@ -1,22 +1,51 @@
 package org.example;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] arr = array(100);
-        remove(arr,1);
-        for (int cas : arr) System.out.println(cas);
-
-
-        sc.nextInt();
-
-        System.out.println(search(arr, 3));
+        int[] arr = null;
+        while (true){
+            try{
+                switch (input("""
+                Ingresa una de las siguientes opciones:
+                1.- Crear un arreglo de 100 espacios con valores aleatorios
+                2.- Imprimir el arreglo
+                3.- Buscar un numero en el arreglo
+                4.- Eliminar un numero del arreglo
+                5.- Salir
+                """)){
+                    case 1:
+                        arr = randarray(100);
+                        break;
+                    case 2:
+                        for(int i=0;i<arr.length;i++){
+                            if(i!=arr.length-1) System.out.print(arr[i]+" ");
+                            else System.out.print(arr[i]+"\n");
+                        }
+                        break;
+                    case 3:
+                        int index = search(arr,input("Ingresa el numero a buscar:"));
+                        if(index != -1){
+                            System.out.println("El indice del numero es: "+index);
+                        }
+                        break;
+                    case 4:
+                        remove(arr,input("Ingresa el numero a buscar:"));
+                        break;
+                    case 5:
+                        System.out.println("Saliendo...");
+                        return;
+                }
+            }catch(NullPointerException ex){
+                System.out.println("Debes crear el arreglo primero");
+            }
+        }
     }
 
-    public static int[] array(int length){
+    public static int[] randarray(int length){
         Random random = new Random();
         int[] arr = new int[length];
         for(int i = 0;i<length;i++) {
@@ -33,19 +62,44 @@ public class Main {
                 break;
             }
         }
+        if (index == -1) System.out.println("Número no encontrado");
         return index;
     }
 
     public static void remove(int[]arreglo, int numero){
-        for (int i=0;(i < arreglo.length)&&(arreglo[i] != -1);i++){
-            if (arreglo[i] == numero){
-                arreglo[i] = -1;
-                for(int x=0;i < arreglo.length;i++){
-                    //aqui va el recorrido de las casillas
-                }
+        int index = search(arreglo,numero);
+        if(index!=-1){
+            for(int i=index;i<arreglo.length-1;i++){
+                arreglo[i] = arreglo[i+1];
+            }
+            arreglo[arreglo.length-1] = -1;
+        }
+    }
+
+    public static int input(String mensaje) {
+        Scanner scanner = new Scanner(System.in);
+        int scanned;
+        while (true) {
+            try {
+                System.out.print(mensaje);
+                scanned = scanner.nextInt();
                 break;
+            } catch (InputMismatchException ex) {
+                System.out.println("Error. Caracteres introducidos no válidos. Intente nuevamente.");
+                scanner.nextLine();
+                wait(2000);
             }
         }
-
+        return scanned;
     }
+
+    public static void wait(int milliseconds) {
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            System.out.println("La espera fue interrumpida.");
+        }
+    }
+
+
 }
--- Python/Data Structures and Algorithms/Actividad1.1.py
+++ Python/Data Structures and Algorithms/Actividad1.1.py
@@ -1,18 +1,51 @@
-from random import randint
-from numpy import array
-
-def remove(arreglo,index:int):
-    new_array = array([arreglo[i] for i in range(len(arreglo)) if i != index])
-    return new_array
+from numpy import array ; from random import randint
 
 def search(arreglo,busqueda):
+    index = -1
     for i in range(len(arreglo)):
         if arreglo[i] == busqueda:
-            return i
-    return -1
+            index = i
+    if index == -1: print("El numero no fue encontrado.")
+    return index
 
-arreglo = array([randint(1,100000) for i in range (100)])
-print(arreglo)
-arreglo = remove(arreglo,2)
-print(arreglo)
-print(search(arreglo,99))
+def remove(arreglo,busqueda:int):
+    index = search(arreglo,busqueda)
+    if index > -1:
+        for i in range(index,len(arreglo)-1):
+            arreglo[i] = arreglo[i+1]
+        arreglo[-1] = -1
+        print("Elemento eliminado exitosamente.")
+
+def randarray(length:int):
+    return array([randint(1,100000) for i in range (length)])
+
+class act:
+    def __init__(self):
+        arr = None
+        while True:
+            match int(input("""
+Ingresa una de las siguientes opciones:
+1.- Crear un arreglo de 100 espacios con valores aleatorios
+2.- Imprimir el arreglo
+3.- Buscar un numero en el arreglo
+4.- Eliminar un numero del arreglo
+5.- Salir
+                """)):
+                case 1:
+                    arr = randarray(100)
+                    print("Arreglo creado exitosamente.")
+                case 2:
+                    print(arr)
+                case 3:
+                    index = search(arr,int(input("Ingresa un numero: ")))
+                    print(f"El indice del valor es: {index}" if index != -1 else "")
+                case 4:
+                    remove(arr,int(input("Ingresa un numero: ")))
+                case 5:
+                    print("Saliendo...")
+                    return
+
+
+
+if __name__ == "__main__":
+    act()
