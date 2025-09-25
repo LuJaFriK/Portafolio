70a4110b9f47c5ea0dbd78fff7db57cffb889fc8
LuJaFriK <donfrijolex@gmail.com>
2025-09-24 23:05:36 -0600

    Update operaciones.py

--- Python/Data Structures and Algorithms/postorden/operaciones.py
+++ Python/Data Structures and Algorithms/postorden/operaciones.py
@@ -1,26 +1,45 @@
-class operators:
-    def __init__(self,expresion:str) -> None:
-        self.__expresion = expresion
-        self.__postorden:str = ''
-    
-    @staticmethod
-    def is_operador(char:str)->bool:
-        return (char in ('*','/','+','-'))
-    
-    @staticmethod
-    def operar(operador:str,a:float,b:float) :
-        match(operador):
-            case '+':
-                return a+b
-            case '-':
-                return a-b
-            case '*':
-                return a*b
-            case '/':
-                return a/b
-        return 0.0
-    
-    @staticmethod
-    def jerarquia(char:str) -> bool:
 
+def operar(operador:str,a:float,b:float) :
+    match(operador):
+        case '+':
+            return a+b
+        case '-':
+            return a-b
+        case '*':
+            return a*b
+        case '/':
+            return a/b
+    return 0.0
+    
+
+def con(expresion:str):
+    postorden:str = ''
+    operadores:list[str] = []
+    operandos:list[str] = []
+
+    i=0
+    numbers = 0
+    for char in expresion:
+        if char in ('*','/','+','-'):
+            operadores.append(char)
+        try:
+            n = float(char)
+            operandos.append(char)
+        except ValueError:
+            pass
+
+        if len(operandos)>1:
+            postorden+=operandos.pop()
+            postorden+=operandos.pop()
+            postorden+=operadores.pop()
+
+    print(operadores)
+    print(operandos)
+    return postorden
+
+
+
+h = ('1+3-4')
+
+print(con(h))
 
