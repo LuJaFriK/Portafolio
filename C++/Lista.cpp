5e66e889953b57bde67c84e2742f4a5bd472854e
LuJaFriK <donfrijolex@gmail.com>
2025-10-13 22:35:26 -0600

    Update Lista.cpp

--- C++/Lista.cpp
+++ C++/Lista.cpp
@@ -23,32 +23,64 @@ class Lista{
 
         bool empty()const{return head == nullptr;}
 
-        static Nodo* order(Nodo* nodo,std::string nombre){
+        Nodo* order(std::string nombre){
+            //Valida primero las puntas de la lista para evitar validaciones extras
+            if(empty()) return nullptr;
+            if(head->value < nombre) return head;
+            if(back->value > nombre) return back;
             
-            if(nodo == nullptr) return new Nodo(nombre);
-
-            if(nombre > nodo->value) nodo->front = order(nodo->front, nombre);
-
-            return 
-
+            Nodo* nodo = head;
+            //Recorre la lista hasta encontrar el lugar del nuevo nodo
+            while(nodo->value < nombre && nodo->front != nullptr){
+                nodo = nodo->front;
+            }
+            return nodo;
         }
-
     public:
 
         Lista():head(nullptr),back(nullptr){}
 
         void add(std::string name){
-
+            //Encontrar el lugar del nuevo nodo
+            Nodo* nodo = order(name);
+            //Si el lugar es nullptr, entonces el nuevo nodo es la cabeza y/o la cola
+            if(nodo == nullptr){
+                nodo = new Nodo(name);
+                head = nodo;
+                back = nodo;
+            }else{
+                //Obtener los nodos adyacentes
+                Nodo* front = nodo->front;
+                Nodo* prev = nodo->prev;
+                //Crear el nuevo nodo
+                Nodo* nuevo = new Nodo(name);
+                //Enlazar los nodos
+                link(prev,nuevo,false);
+                link(nuevo,front,true);
+                //Actualizar las puntas de la lista
+                if (prev == nullptr) head = nuevo;
+                if (front == nullptr) back = nuevo;
+            }
         }
 
-        void get(int name)const{
-            
+        std::string get(int index)const{
+            if(index < 0 || empty()) throw std::out_of_range("Índice inválido");
+            Nodo* nodo = head;
+            for(int i = 0; i < index; i++) nodo = nodo->front;
+
+            if(nodo == nullptr) throw std::out_of_range("Índice inválido");
+
+            return nodo->value;
         }
 
         void remove(std::string name){
-            Nodo* del;
-
-
+            Nodo* del = head;
+            while(del->value != name){
+                del = del->front;
+            }
+            link(del->prev,del->front,true);
+            if (del == head) head = del->front;
+            if (del == back) back = del->prev;
             delete del;
         }
 
