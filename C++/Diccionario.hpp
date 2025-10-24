ed6fa3f34319f096fec3c12663bae0b8dc72138c
LuJaFriK <donfrijolex@gmail.com>
2025-10-23 21:16:19 -0600

    Diccionario comentado

--- C++/Diccionario.hpp
+++ C++/Diccionario.hpp
@@ -10,64 +10,76 @@ class Diccionario {
     private:
 
         Nodo_dict<U,T>* search(const U& name){
+            //Si la lista está vacía, no hay nada que buscar
             if(head == nullptr) return nullptr;
-
+            //Buscar en la lista
             Nodo_dict<U,T>* current = head;
             do{
                 if(current->address == name) return current;
                 current = current->getNext();
             }while(current != head);
-
+            //Si no encontra nada, devuelve nullptr
             return nullptr;
         }
         
     protected:
+        //Punteros
         Nodo_dict<U,T>* head;
         Nodo_dict<U,T>* back;
     public:
         Diccionario():head(nullptr),back(nullptr){}
-
-        ~Diccionario(){}
-
+        
+        //Destructor virtual por si se atora una herencia
+        virtual ~Diccionario(){ clear(); }
+        
+        //Constructor de copia
         Diccionario(const Diccionario& other)
         :head(new Nodo_dict<U,T>(other.head->address,other.head->getValue())),back(nullptr){
-            
+            //Enlazar punteros
             link(back,head);
             Nodo_dict<U,T>* other_current = other.head->getNext();
             Nodo_dict<U,T>* current = head->getNext();
 
             do{
+                //Asignar memoria y enlazar
                 Nodo_dict<U,T>* nodo = new Nodo_dict<U,T>(other_current->address,other_current->getValue());
                 link(current,nodo);
-
+                //Avanzar punteros
                 current = current->getNext();
                 other_current = other_current->getNext();
+            
+            //El ciclo termina antes de llegar al inicio
             }while(other_current != other.head);
+            //Apuntar back al último nodo
             back = current;
         }
         
-        bool empty(){ return head == nullptr; }
+        bool empty()const{ return head == nullptr; }
 
         void set(const U& name, const T& value){
             Nodo_dict<U,T>* to_set = search(name);
+            //Caso existente
             if(to_set) {
                 to_set->setValue(value);
+            //Caso no existente
             }else{
                 Nodo_dict<U,T>* nuevo = new Nodo_dict<U,T>(name,value);
                 nuevo->setValue(value);
-                
+                //Caso vacío
                 if(!head){
                     head = nuevo;
                     back = nuevo;
+                //Caso encolar
                 }else{
                     link(back, nuevo);
                     back = nuevo;
                 }
+                //De cualquier modo, enlazar el nuevo nodo con el primero
                 link(back,head);
             }
         }
 
-        T get(const U& name){
+        T get(const U& name)const{
             //Buscar el nodo
             Nodo_dict<U,T>* nodo = search(name);
             //Retornar un valor válido
@@ -76,21 +88,23 @@ class Diccionario {
         }
 
         void remove(const U& name){
+            //Buscar el nodo
             Nodo_dict<U,T>* del = search(name);
+            //Si el nodo existe
             if(del){
-
+                //Caso eliminar el único elemento
                 if(del == head && del == back) head = back = nullptr;
-            
+                //Caso eliminar el primer o último elemento
                 else if(del == head) head = head->getNext();
                 else if(del == back) back = back->getPrev();
                 
-
+                //Caso nodo intermedio
                 if(head) link(del->getPrev(),del->getNext());
-                
+                //Finalmente, eliminar el nodo
                 delete del;
             }
         }
-        
+        //Limpiar el diccionario
         void clear(){
             Nodo_dict<U,T>* current = head;
             Nodo_dict<U,T>* next = nullptr;
@@ -102,7 +116,7 @@ class Diccionario {
             }
             head = back = nullptr;
         }
-
+        //Mostrar el diccionario
         std::string mostrar(){ return Nodo_to_string(head); }
 
 };
